8f1c115838cd9508174c1af0c590cb9360c63fb0
Hasan <hassandossa5@gmail.com>
2026-01-26 11:24:55 +0300

    fix: import

--- backend/utils/passport.strategy.local.js
+++ backend/utils/passport.strategy.local.js
@@ -1,6 +1,6 @@
 import passport from "passport";
 import { Strategy as LocalStrategy } from "passport-local";
-import { prisma } from "../lib/dbConnect";
+import { prisma } from "../lib/dbConnect.js";
 import bcrypt from "bcrypt"
 
 passport.use("local", new LocalStrategy({
